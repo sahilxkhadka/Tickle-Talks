3e1eb794b5186d44d42097f6d3e56ee0864efa57
Sahil Khadka <sahilkhadka767@gmail.com>
2024-04-26 20:00:46 +0545

    added not found file

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,9 +1,11 @@
+import { notFound } from "next/navigation";
+
 export const getRandomDogImage = async () => {
 	try {
 		const res = await fetch("https://random.dog/woo.json");
 		const data: RandomDogImgResponse = await res.json();
 		return data.url;
 	} catch (err) {
-		throw new Error("Error Fetching dog image");
+		notFound();
 	}
 };
