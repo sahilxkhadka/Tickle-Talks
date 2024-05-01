9dc9f42130161aca15a119d131ac1ade7e40037f
Sahil Khadka <sahilkhadka767@gmail.com>
2024-05-01 22:58:06 +0545

    not-found page changed

--- app/dog-facts/not-found.tsx
+++ app/dog-facts/not-found.tsx
@@ -1,11 +1,26 @@
+import dogSwag from "@/public/images/dog-swag.png";
+import { revalidatePath } from "next/cache";
+import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function NotFound() {
+export default async function NotFound() {
+	const revalidate = async () => {
+		"use server";
+		revalidatePath("/dog-facts");
+		redirect("/");
+	};
 	return (
-		<div>
-			<h2>Not Found</h2>
-			<p>Could not find requested resource</p>
-			<Link href='/'>Return Home</Link>
+		<div className='w-fit mx-auto'>
+			<Image src={dogSwag} alt='Swaggy dog' placeholder='blur' />
+			<p className='my-6 text-center text-2xl font-semibold'>
+				BRB after the smoke break
+			</p>
+			<form action={revalidate}>
+				<button className='button mx-auto block' type='submit'>
+					Hit me up again
+				</button>
+			</form>
 		</div>
 	);
 }
