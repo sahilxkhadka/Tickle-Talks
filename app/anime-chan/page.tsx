fcfdf5204afb0908257e2756db2d0fab98e63abf
Sahil Khadka <sahilkhadka767@gmail.com>
2024-05-03 09:38:22 +0545

    UI created for displaying quote

--- app/anime-chan/page.tsx
+++ app/anime-chan/page.tsx
@@ -1,5 +1,4 @@
 import { getAnimeQuote } from "@/lib/actions";
-import React from "react";
 
 export default async function AnimeChan() {
 	const animeQuote = await getAnimeQuote();
@@ -11,6 +10,11 @@ export default async function AnimeChan() {
 				<span className='text-lg font-semibold'>{character}</span> from{" "}
 				<span className='text-lg font-semibold'>{anime}</span>
 			</h2>
+			<div className='px-12 py-16 bg-white/50 backdrop-blur-md rounded-md shadow-quote mt-14'>
+				<p className='text-2xl text-pretty font-extrabold italic bg-gradient-to-r from-blue-600 via-purple-400 to-green-500 text-transparent bg-clip-text'>
+					&#34;{quote}&#34;
+				</p>
+			</div>
 		</div>
 	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			boxShadow: {
+				quote: "rgba(0, 0, 0, 0.1) 0px 10px 50px;",
+			},
 			animation: {
 				"text-display": "textDisplay 1.5s linear",
 			},
