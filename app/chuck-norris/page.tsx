09c53ceb4c483e4d1766a0d8fb21eb48a7f54ce3
Sahil Khadka <sahilkhadka767@gmail.com>
2024-05-04 19:04:18 +0545

    minors

--- app/chuck-norris/page.tsx
+++ app/chuck-norris/page.tsx
@@ -4,5 +4,5 @@ export default async function ChuckNurris() {
 	const res = await fetch("https://api.chucknorris.io/jokes/random");
 	const data: JokesResponse = await res.json();
 	console.log(data);
-	return <div>ChuckNurris</div>;
+	return <div>{data.value}</div>;
 }
