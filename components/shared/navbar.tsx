72cdbce44d2c7627d037f21f92e4cd538df7c6ae
Sahil Khadka <sahilkhadka767@gmail.com>
2024-05-08 18:49:10 +0545

    nav for mobile

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
 	const pathname = usePathname();
 	return (
 		<nav className='max-w-4xl mx-4 sm:mx-auto py-6 mt-6 shadow-xl rounded-md sm:rounded-full bg-pink-200 px-10'>
-			<ul className='flex justify-center gap-8 flex-wrap'>
+			<ul className='flex justify-start sm:justify-center gap-8 sm:flex-wrap overflow-auto hide-scrollbar'>
 				{navItems.map((navItem) => {
 					const isActive = pathname === navItem.link;
 					return (
@@ -17,7 +17,7 @@ export default function Navbar() {
 							<Link
 								href={navItem.link}
 								className={cn(
-									"font-medium text-sm px-9 py-3.5 rounded-md",
+									"font-medium text-sm px-9 block py-3.5 rounded-md whitespace-nowrap",
 									isActive ? "bg-white shadow-sm" : "hover:bg-white"
 								)}
 							>
