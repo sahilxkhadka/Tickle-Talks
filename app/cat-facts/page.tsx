b67175ee305cdbeb93b753c18115fda329e00b6d
Sahil Khadka <sahilkhadka767@gmail.com>
2024-05-09 22:26:53 +0545

    metadata added

--- app/cat-facts/page.tsx
+++ app/cat-facts/page.tsx
@@ -4,6 +4,12 @@ import Cat from "@/components/icons/cat";
 import NextBtn from "@/components/shared/next-btn";
 import { catFacts } from "@/constants/cat-facts";
 import { revalideCatFacts } from "@/lib/actions";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Cat Facts",
+	description: "Random facts and images of cats.",
+};
 
 export default function CatFacts() {
 	const randomIndex = Math.ceil(Math.random() * 9);
@@ -13,7 +19,7 @@ export default function CatFacts() {
 			<div className='text-center mb-3 sm:mb-6 from-purple-400 bg-gradient-to-r text-xl sm:text-3xl font-extrabold to-orange-400 bg-clip-text text-transparent animate-text-display'>
 				{randomCatFact}
 			</div>
-			<div className='my-3 sm:my-6 flex gap-3 items-center justify-end pr-12'>
+			<div className='my-3 sm:my-6 flex gap-3 items-center justify-end pr-2 sm:pr-12'>
 				<Author />
 				<p className='font-semibold'>ra_gya00</p>
 			</div>
